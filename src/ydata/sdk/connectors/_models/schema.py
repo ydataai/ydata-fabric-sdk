af9b88a4a7b428ac82138371886f39d46b7364cd
Luís Portela Afonso <luis.portela@ydata.ai>
2025-02-03 21:20:22 +0000

    fix: pydantic and typeguard

--- src/ydata/sdk/connectors/_models/schema.py
+++ src/ydata/sdk/connectors/_models/schema.py
@@ -2,16 +2,17 @@ from typing import List, Optional
 
 from pydantic import Field
 
-from ydata.sdk.common.model import BaseModel
+from ydata.sdk.common.model import BaseModel, Config
 from ydata.sdk.common.pydantic_utils import to_camel
 
 
-class BaseConfig(BaseModel.Config):
+class BaseConfig(Config):
     alias_generator = to_camel
 
 
 class TableColumn(BaseModel):
     """Class to store the information of a Column table."""
+    model_config = BaseConfig()
 
     name: str
     variable_type: str  # change this to the datatypes
@@ -20,24 +21,21 @@ class TableColumn(BaseModel):
     foreign_keys: list
     nullable: bool
 
-    Config = BaseConfig
 
 
 class Table(BaseModel):
     """Class to store the table columns information."""
+    model_config = BaseConfig()
 
     name: str
     columns: List[TableColumn]
     primary_keys: List[TableColumn]
     foreign_keys: List[TableColumn]
 
-    Config = BaseConfig
-
 
 class Schema(BaseModel):
     """Class to store the database schema information."""
+    model_config = BaseConfig()
 
     name: str
     tables: Optional[List[Table]] = Field(None)
-
-    Config = BaseConfig
